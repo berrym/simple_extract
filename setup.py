8d6a80e743c8caaf7725b296626318241cba6fe3
Michael Berry <trismegustis@gmail.com>
2021-02-27 01:36:21 -0500

    Updated setup.py, version bump to 0.1.2

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
     name="simple_extract",
-    version="0.1.1",
-    packages=[""],
+    version="0.1.2",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
     url="https://github.com/berrym/simple_extract.git",
-    license="MIT",
     author="Michael Berry",
     author_email="trismegustis@gmail.com",
-    description="A small command line utility to help extract compressed archives",
+    description="A small command line utility to help extract compressed archives.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     entry_points={
         "console_scripts": [
             "simple-extract = simple_extract:main",
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
